b1426d8fc28d32b96448afcb42ba985f5d665c9c
王树根 <wsg1107556314@163.com>
2018-12-12 20:28:31 +0800

    加入了detok脚本
    Signed-off-by: 王树根 <wsg1107556314@163.com>

---
+++ detok.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: 王树根
+# email: wangshugen@ict.ac.cn
+# date: 2018-12-12 20:08
+import os
+import subprocess
+
+from arguments import parse_convert_arguments
+
+
+def main(args):
+    if args.output_path is None:
+        setattr(args, 'output_path', '{}.untok'.format(getattr(args, 'file_path')))
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    kwargs = {
+        'class_path': '{}/stanford-postagger-3.9.1.jar'.format(script_path),
+        'main_class': 'edu.stanford.nlp.process.PTBTokenizer',
+        'input': args.file_path,
+        'output': args.output_path,
+        'options': ' '.join([
+            '-untok',
+        ])
+    }
+    cmd_str = 'java -cp {class_path} {main_class} {options} < {input} > {output}'.format(**kwargs)
+    if args.verbose:
+        print(cmd_str)
+    subprocess.call(cmd_str, shell=True)
+
+
+if __name__ == '__main__':
+    arguments = parse_convert_arguments()
+    main(arguments)
