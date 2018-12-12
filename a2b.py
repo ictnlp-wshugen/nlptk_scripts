f857b1b56d104b2a8c9b8f77ccf9b181e5bda536
王树根 <wsg1107556314@163.com>
2018-12-12 21:22:02 +0800

    a2b修改为绝对路径
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- a2b.py
+++ a2b.py
@@ -3,6 +3,7 @@
 # author: 王树根
 # email: wangshugen@ict.ac.cn
 # date: 2018-12-12 19:54
+import os
 import subprocess
 
 from options import parse_convert_arguments
@@ -11,8 +12,17 @@ from options import parse_convert_arguments
 def main(args):
     if args.output_path is None:
         setattr(args, 'output_path', '{}.tok'.format(args.file_path))
-    option = 'pre' if args.lang == 'zh' else 'all'
-    cmd_str = './tra2b -{} {} {} {}'.format(args.encoding, option, args.file_path, args.output_path)
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    kwargs = {
+        'command': '{}/tra2b'.format(script_path),
+        'input': args.file_path,
+        'output': args.output_path,
+        'options': ' '.join([
+            '-{}'.format(args.encoding),
+            '-{}'.format('pre' if args.lang == 'zh' else 'all')
+        ])
+    }
+    cmd_str = '{command} {options} {input} {output}'.format(**kwargs)
     if args.verbose:
         print(cmd_str)
     subprocess.check_call(cmd_str, shell=True)
