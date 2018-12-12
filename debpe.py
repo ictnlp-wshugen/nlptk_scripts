221692b525ad0fd97498b89cf5558a8fafb1248d
王树根 <wsg1107556314@163.com>
2018-12-12 21:04:44 +0800

    加入了移除bpe的脚本
    Signed-off-by: 王树根 <wsg1107556314@163.com>

---
+++ debpe.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: 王树根
+# email: wangshugen@ict.ac.cn
+# date: 2018-12-12 20:58
+import subprocess
+
+from options import parse_convert_arguments
+
+
+def main(args):
+    if args.output_path is None:
+        setattr(args, 'output_path', '{}.debpe'.format(args.file_path))
+    kwargs = {
+        'input': args.file_path,
+        'output': args.output_path
+    }
+    cmd_str = "sed -r 's/(@@ )|(@@ ?$)//g' {input} > {output}".format(**kwargs)
+    if args.verbose:
+        print(cmd_str)
+    subprocess.call(cmd_str, shell=True)
+
+
+if __name__ == '__main__':
+    arguments = parse_convert_arguments()
+    main(arguments)
